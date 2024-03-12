cf4b540319d045ad5f025b9a784f351d8aaa2a37
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-03-12 08:33:40 +0100

    feat:Sets and exercise

---
+++ Set.dart
@@ -0,0 +1,10 @@
+void main() {
+  var elements = {'O', 'N', 'Pb', 'Fe'};
+  print(elements);
+
+  elements.add('Cu');
+  print(elements);
+
+  elements.addAll(['C', 'Zn', 'Ag']);
+  print(elements);
+}
