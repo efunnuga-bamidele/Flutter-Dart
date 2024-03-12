5a4bfda5a20e935280f448b31f127520a4f0cc7c
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-03-12 09:24:22 +0100

    feat:Const & Final

---
+++ const-final.dart
@@ -0,0 +1,8 @@
+void main() {
+  var speed = 100;
+  final maxSpeed = speed * 4; //Can do runtime initialization using calculation.
+  print(maxSpeed);
+
+  const version = '1.0.0'; //Must be set before runtime.
+  print(version);
+}
