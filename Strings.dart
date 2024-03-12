58adb85c5fc5f3f8088b741bf451cc112b2f6fdd
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-03-12 06:48:52 +0100

    feat:Strings and String Exercise

---
+++ Strings.dart
@@ -0,0 +1,7 @@
+void main() {
+  String myName = "Bamidele Efunnuga";
+  int myAge = 35;
+
+  print('My name is $myName, and i am $myAge years old.');
+  print('The length of my name is ${myName.length} characters long.');
+}
