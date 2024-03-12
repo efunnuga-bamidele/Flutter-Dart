ea91bd50dabe7cc881dcab9e1fb796caefddfe95
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-03-12 06:40:51 +0100

    feat:Exercise on Numbers

---
+++ 03-Exercise-Numbers.dart
@@ -0,0 +1,11 @@
+void main() {
+//  Given the double below, assign it
+//  to a new variable where its rounded to the
+//  nearest integer.
+  var someNum = 3.1;
+  print(someNum.ceil());
+  print(someNum.floor());
+  print(someNum.round());
+}
+// BONUS QUESTION; In the cases of 0.5, how can you
+// ensure its rounded up or down?
