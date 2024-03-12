31046f478c7227c13874265b96ca9171226a560a
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-03-12 06:35:26 +0100

    feat: Number types, convertion of types, variable type check

--- dataTypes.dart
+++ dataTypes.dart
@@ -4,4 +4,21 @@ void main() {
   print(str1.toLowerCase());
   print(str1.toUpperCase());
   print(str1.contains(RegExp('a'), 0));
+
+  //Number Data Types
+  int numberOne = 12;
+  double numberTwo = 2.55;
+  print(numberOne / numberTwo);
+
+  //exponentials
+  var expNum = 1.2e4;
+  print(expNum);
+
+  //Type Check
+  var stringNumber = "14.6";
+  print(stringNumber.runtimeType);
+
+  //converted type
+  var convertedNumber = double.parse(stringNumber);
+  print(convertedNumber.runtimeType);
 }
