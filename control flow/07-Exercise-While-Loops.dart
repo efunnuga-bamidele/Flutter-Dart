f9cfd9647dcb07f89a69509fea51d6cecb1ca35c
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-03-13 07:40:30 +0100

    feat:while loop

---
+++ control flow/07-Exercise-While-Loops.dart
@@ -0,0 +1,11 @@
+// Write a Dart program that prints the
+//  first 10 positive even numbers using a while loop.
+void main() {
+  int i = 1;
+  int x = 0;
+  while (i <= 10) {
+    x += 2;
+    print(x);
+    i++;
+  }
+}
