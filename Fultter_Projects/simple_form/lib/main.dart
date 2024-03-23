9d1ccf160c53c035e664df9659fb6eb30750ab01
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-03-23 19:11:46 +0100

    feat:Update simple form

--- Fultter_Projects/simple_form/lib/main.dart
+++ Fultter_Projects/simple_form/lib/main.dart
@@ -1,1 +1,47 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //Initial Variable
+  String _inputText = '';
+
+  //Widget --> setState() --> update variable --> widget state element
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Home Page'),
+      ),
+      body: Center(
+        child: TextField(
+          decoration: const InputDecoration(
+            hintText: "Enter some text here",
+          ),
+          onChanged: (value) => setState(() => _inputText = value),
+        ),
+      ),
+      bottomSheet: Container(
+        alignment: Alignment.center,
+        height: 50,
+        child: Text('Yor typed $_inputText'),
+      ),
+    );
+  }
+}
