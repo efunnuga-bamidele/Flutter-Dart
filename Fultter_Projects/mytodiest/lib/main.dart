7511f1c3b9b6be22eddffe437503dd756a0a6920
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-03-23 13:25:37 +0100

    feat:Create a Styled Button Container

--- Fultter_Projects/mytodiest/lib/main.dart
+++ Fultter_Projects/mytodiest/lib/main.dart
@@ -1,25 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.blueGrey,
-        ),
-        home: MyApp(),
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.blueAccent,
       ),
-    );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Todiest"),
+        ),
+        body: Center(
+          child: UserButton(),
+        ),
+      )));
+}
 
-class MyApp extends StatelessWidget {
+class UserButton extends StatelessWidget {
+  // const UserButton(super key);
+
   @override
   Widget build(BuildContext context) {
-    return (Scaffold(
-      appBar: AppBar(
-        title: const Text("My Todiest"),
+    return GestureDetector(
+      onTap: () {
+        // TODO: Add functionality
+        print('User button was tapped');
+      },
+      child: Container(
+        height: 60,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.redAccent,
+        ),
+        child: const Center(
+          child: Text("Click ME", style: TextStyle(color: Colors.white)),
+        ),
       ),
-      body: const Center(
-        child: Text("This is my main app"),
-      ),
-    ));
+    );
   }
 }
