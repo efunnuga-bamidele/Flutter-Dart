6aedcdc85ecdf573f8557f70095f0d7c59a54c8b
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-03-23 14:15:48 +0100

    feat:Row Component

--- Fultter_Projects/rows_and_columns/lib/main.dart
+++ Fultter_Projects/rows_and_columns/lib/main.dart
@@ -5,38 +5,44 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text("Rows and Columns")),
-        body: MyWidget(),
+        body: const MyWidget(),
       ),
     ),
   );
 }
 
 class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 100,
-          height: 100,
-          color: Colors.redAccent,
-        ),
-        Container(
-          width: 100,
-          height: 100,
-          color: Colors.greenAccent,
-        ),
-        Container(
-          width: 100,
-          height: 100,
-          color: Colors.blueAccent,
-        ),
-        Container(
-          width: 100,
-          height: 100,
-          color: Colors.yellowAccent,
-        ),
-      ],
+    return Center(
+      child: Row(
+        //change to Column from Row
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.redAccent,
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.greenAccent,
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.blueAccent,
+          ),
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.yellowAccent,
+          ),
+        ],
+      ),
     );
   }
 }
