d24209a57fb6df74ae232f3efe1739d604f3de71
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-03-25 16:41:29 +0100

    feat:Build multiple screen

--- Fultter_Projects/multi_screen/lib/main.dart
+++ Fultter_Projects/multi_screen/lib/main.dart
@@ -1,1 +1,55 @@
+import "package:flutter/material.dart";
 
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'App',
+      home: FirstScreen(),
+    ),
+  );
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          },
+          child: const Text('Go to second screen'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go to first screen'),
+        ),
+      ),
+    );
+  }
+}
