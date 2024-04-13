85d41feb4f14a2ada995c001fc8c9090d409ac89
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-04-13 08:19:37 +0100

    Button Styling

--- Fultter_Projects/textfield_and_styling/lib/main.dart
+++ Fultter_Projects/textfield_and_styling/lib/main.dart
@@ -38,11 +38,39 @@ class MyCustomForm extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          const TextField(),
-          TextFormField(),
+          const SizedBox(height: 16),
+          const TextField(
+              style: TextStyle(color: Colors.blue, fontSize: 20),
+              decoration: InputDecoration(
+                hintText: 'Enter your name',
+                prefixIcon: Icon(Icons.person, size: 30),
+              )),
+          const SizedBox(height: 16),
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: 'Enter your name',
+                labelStyle: const TextStyle(color: Colors.blue),
+                filled: true,
+                fillColor: Colors.white,
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(8))),
+            style: const TextStyle(color: Colors.blue),
+          ),
+          const SizedBox(height: 16),
           ElevatedButton(
-            child: const Text('Submit'),
             onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.teal,
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(
+                fontSize: 18,
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            child: const Text('Submit'),
           )
         ],
       ),
