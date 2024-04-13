a6cf8c04d6d9bf4e285afb8b9b5adb31538315b8
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-04-13 07:47:21 +0100

    feat: SingleScrollView Implementation

--- Fultter_Projects/textfield_and_styling/lib/main.dart
+++ Fultter_Projects/textfield_and_styling/lib/main.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = 'Form Styling Demo';
+
+    return MaterialApp(
+      title: 'MyApp',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+        brightness: Brightness.light,
+        fontFamily: 'Roboto',
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle, style: TextStyle(color: Colors.white)),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: const MyCustomForm(),
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatelessWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const TextField(),
+          TextFormField(),
+          ElevatedButton(
+            child: const Text('Submit'),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
