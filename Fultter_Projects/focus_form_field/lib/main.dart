4e8b5fcc19c1cec3351961ec9cbbdfb4215c896c
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-04-13 09:15:38 +0100

    feat:Handle Focus

--- Fultter_Projects/focus_form_field/lib/main.dart
+++ Fultter_Projects/focus_form_field/lib/main.dart
@@ -1,1 +1,87 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Focus Field Demo',
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primaryColor: Colors.blueAccent,
+          fontFamily: 'Roboto'),
+      home: const MyFormPage(),
+    );
+  }
+}
+
+class MyFormPage extends StatefulWidget {
+  const MyFormPage({super.key});
+
+  @override
+  State<MyFormPage> createState() => _MyFormPageState();
+}
+
+class _MyFormPageState extends State<MyFormPage> {
+  late FocusNode myFocusNode;
+
+// Manages the lifecycle of a focus node
+  @override
+  void initState() {
+    super.initState();
+    myFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Focus Field Demo'),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+          foregroundColor: Colors.white,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                const SizedBox(height: 10),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'First Name'),
+                  autofocus: true,
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Last Name'),
+                  focusNode: myFocusNode,
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Email Address'),
+                ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            myFocusNode.requestFocus();
+          },
+          tooltip: 'Click to focus cursor',
+          child: const Icon(Icons.star),
+        ));
+  }
+}
